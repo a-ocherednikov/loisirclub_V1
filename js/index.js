273fc56773881753aa7047cbded30e13f6ea5719
Andrey <a.ocherednikov@gmail.com>
2021-03-24 15:01:55 +0000

    JS Options

---
+++ js/index.js
@@ -0,0 +1,18 @@
+const countDownDate = new Date("Apr 25, 2021 18:00:00").getTime();
+
+const x = setInterval(function() {
+  const now = new Date().getTime();
+  let distance = countDownDate - now;
+  const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+  document.getElementById("countdown").innerHTML = days + " д " + hours + " г "
+  + minutes + " хв " + seconds + " сек ";
+
+  if (distance < 0) {
+    clearInterval(x);
+    document.getElementById("countdown").innerHTML = "Івент нажаль пройшов";
+  }
+}, 1000);
