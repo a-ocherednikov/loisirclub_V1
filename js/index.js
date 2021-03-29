17464604b7548eaac9be26a509351505e4c5f49d
Andrey <79542447+a-ocherednikov@users.noreply.github.com>
2021-03-29 17:50:46 +0300

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,18 +1,17 @@
 const countDownDate = new Date("Apr 25, 2021 18:00:00").getTime();
 
 const num = setInterval(function() {
-  const now = new Date().getTime();
-  let distance = countDownDate - now;
-  const days = Math.floor(distance / (1000 * 60 * 60 * 24));
-  const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-  const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-  const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+    const now = new Date().getTime();
+    let distance = countDownDate - now;
+    const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    const seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
-  document.getElementById("countdown").innerHTML = days + " д " + hours + " г "
-  + minutes + " хв " + seconds + " сек ";
+    document.getElementById("countdown").innerHTML = days + " д " + hours + " г " + minutes + " хв " + seconds + " сек ";
 
-  if (distance < 0) {
-    clearInterval(num);
-    document.getElementById("countdown").innerHTML = "Івент нажаль пройшов";
-  }
+    if (distance < 0) {
+        clearInterval(num);
+        document.getElementById("countdown").innerHTML = "Івент нажаль пройшов";
+    }
 }, 1000);
